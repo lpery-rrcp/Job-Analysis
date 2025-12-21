3c51752cc8810d1ff9488a91ad1413ceaff9420d
Lance Pery <lpery@academic.rrc.ca>
2025-12-21 12:17:34 -0600

    Can add title, company, and Date to the SQLite database

--- jobAnalytic.py
+++ jobAnalytic.py
@@ -1,15 +1,31 @@
 import sqlite3
 
 
-def add_job():
+def add_job(title, company, date):
     """This function adds Job components to the SQL.
             - Job title
             - Company
             - Date applied
         (Can add more later but the above are essential.)
     """
+    application = [title, company, date]
+    con = sqlite3.connect("JobApplicationData.db")
+    cur = con.cursor()
 
-    return "Job Added"
+    # Checks to see if table is created if not makes it
+    cur.execute('''CREATE TABLE IF NOT EXISTS jobapplications
+                    (id INTEGER PRIMARY KEY, title TEXT NOT NULL, company TEXT NOT NULL, date_applied TEXT NOT NULL)
+                ''')
+
+    # Insert data
+    cur.execute(
+        """INSERT INTO jobapplications (title, company, date_applied) VALUES (?, ?, ?)""", application)
+
+    con.commit()
+    new_id = cur.lastrowid
+    con.close()
+
+    return f"Job Added: {title} | ID: {new_id}"
 
 
 def delete_job():
@@ -27,19 +43,33 @@ def edit_job():
     return "Job edited"
 
 
+def show_database():
+    con = sqlite3.connect("JobApplicationData.db")
+    cur = con.cursor()
+
+    cur.execute("""SELECT * FROM jobapplications""")
+    rows = cur.fetchall()
+
+    con.close()
+    return rows
+
+
 print("1. Add Job Application.")
 print("2. Delete Job Application")
 print("3. Edit Job Application")
-print("4. Exit")
+print("4. Show database")
+print("5. Exit")
 userInput = int(input("Enter number: "))
 
 if userInput == 1:
-    print(add_job())
+    print(add_job("Cain", "CnA", "2017-05-20"))
 elif userInput == 2:
     print(delete_job())
 elif userInput == 3:
     print(edit_job())
 elif userInput == 4:
+    print(show_database())
+elif userInput == 5:
     print("Thanks for using Job Analytics.❤️")
 else:
     print("Invalid entry!! Please use numbers.")
